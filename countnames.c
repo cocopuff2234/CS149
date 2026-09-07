997e4a3b1544504f0f016cf518600aae06a5f8b3
Erik Thompson <erikthompson@Eriks-MacBook-Pro-3.local>
2026-09-07 12:24:35 -0700

    Add countnames starter code

---
+++ countnames.c
@@ -0,0 +1,26 @@
+/**
+ * Description: This program counts the occurrences of each name in a file using a hash table.
+ * Author names: Erik Thompson, Ryuto Kawabata
+ * Author emails: erik.thompson@sjsu.edu, ryuto.kawabata@sjsu.edu
+ * Last modified date: 09/07/2026
+ * Creation date: 09/07/2026
+ **/
+#include <stdio.h>
+#include <string.h>
+
+/* Define table size */
+#define TABLE_SIZE 101
+
+/* Define hash table entry, track name, count, and next pointer*/
+struct Entry {
+    char name[31];
+    int count;
+    struct Entry *next;
+};
+
+int main() {
+    
+
+
+}
+
